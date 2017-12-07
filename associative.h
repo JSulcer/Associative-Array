74d985195fd9f9aba7d8fa7dff42da1d8adc6f70
Adam Chisolm <adachis@ut.utm.edu>
2017-12-07 13:40:13 -0600

    Added comments to associative.h.

--- associative.h
+++ associative.h
@@ -11,12 +11,76 @@ class associative
   associative();
   virtual ~associative();
   void insert(std::string key, int data);
+  /*
+   * Description: 
+   *   Inserts new value into the associative array.
+   *
+   * Inputs:
+   *   key:
+   *     The key for the new data.
+   *   data:
+   *     The data to be inserted.
+   * 
+   */
+  
   void find(std::string key);
+  /*
+   * Description: 
+   *   Finds and prints the value for the given key.
+   *
+   * Inputs:
+   *   key:
+   *     The key for the value you want to find.
+   * 
+   */
+  
   void print();
+  /*
+   * Description: 
+   *   Prints the contents of the associative array in order (sorted by key) to the user as (key, value) pairs.
+   *
+   *  
+   */
+  
   void min();
+  /*
+   * Description: 
+   *   Finds and prints the smallest (leftmost) key in the tree.
+   * 
+   */
+  
   void max();
-  void save_file();
+  /*
+   * Description: 
+   *   Imports the data from the given file into a database.
+   *
+   * Inputs:
+   *   fileName:
+   *     The name of the file with student data.
+   * 
+   */
+  
+  void save_file(std::string fileName);
+  /*
+   * Description: 
+   *   Stores the contents of the tree in order in a file.
+   *
+   * Inputs:
+   *   fileName:
+   *     The name of the file to store the data in.
+   * 
+   */
+  
   void delete_index(std::string key);
+  /*
+   * Description: 
+   *   Removes the first instance of the value from the associative array.
+   *
+   * Inputs:
+   *   key:
+   *     The value you want to delete..
+   * 
+   */
 
  private:
   Node *root;
