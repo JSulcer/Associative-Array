e0065f3f35c1e990f482c049b8de51c7e50f1dd3
JSulcer <34108753+JSulcer@users.noreply.github.com>
2017-12-07 16:41:48 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,5 +9,15 @@ using namespace std;
 int main(){
   cout << "Hello World" << endl;
 
+  associative Test;
+
+  Test.insert("Bob", 5);
+  Test.insert("Ted", 2);
+  Test.insert("Nik", 7);
+
+  Test.print();
+
+  cout << "Done" << endl;
+
   return 0;
 }
