25abbe51d3848dfd6482e342ff118e4153239842
JSulcer <34108753+JSulcer@users.noreply.github.com>
2017-12-07 16:41:22 -0600

    Update associative.h

--- associative.h
+++ associative.h
@@ -8,83 +8,87 @@
 class associative
 {
  public:
+
   associative();
   virtual ~associative();
-  void insert(std::string key, int data);
+  void insert(std::string value, int num);
   /*
-   * Description: 
+   * Description:
    *   Inserts new value into the associative array.
-   *
+   *clea
    * Inputs:
    *   key:
    *     The key for the new data.
    *   data:
    *     The data to be inserted.
-   * 
+   *
    */
-  
+
   void find(std::string key);
   /*
-   * Description: 
+   * Description:
    *   Finds and prints the value for the given key.
    *
    * Inputs:
    *   key:
    *     The key for the value you want to find.
-   * 
+   *
    */
-  
+
   void print();
   /*
-   * Description: 
+   * Description:
    *   Prints the contents of the associative array in order (sorted by key) to the user as (key, value) pairs.
    *
-   *  
+   *
    */
-  
+
   void min();
   /*
-   * Description: 
+   * Description:
    *   Finds and prints the smallest (leftmost) key in the tree.
-   * 
+   *
    */
-  
+
   void max();
   /*
-   * Description: 
+   * Description:
    *   Imports the data from the given file into a database.
    *
    * Inputs:
    *   fileName:
    *     The name of the file with student data.
-   * 
+   *
    */
-  
+
   void save_file(std::string fileName);
   /*
-   * Description: 
+   * Description:
    *   Stores the contents of the tree in order in a file.
    *
    * Inputs:
    *   fileName:
    *     The name of the file to store the data in.
-   * 
+   *
    */
-  
+
   void delete_index(std::string key);
   /*
-   * Description: 
+   * Description:
    *   Removes the first instance of the value from the associative array.
    *
    * Inputs:
    *   key:
    *     The value you want to delete..
-   * 
+   *
    */
+
 
  private:
   Node *root;
-  void insertHelper(Node *current, std::string value);
+  void insertHelper(Node *current, std::string value, int num);
+  void printHelper(Node *current);
+
 };
 
 #endif // ASSOCIATIVE_H
