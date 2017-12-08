46df3eddce9f1dd02a4d0cab2b014006073aabd3
JSulcer <34108753+JSulcer@users.noreply.github.com>
2017-12-07 19:30:41 -0600

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 Node::Node(string value, int num)
 {
-  left = NULL;
-  right = NULL;
-  key  = value;
-  data = num;
+    left = NULL;
+    right = NULL;
+    key  = value;
+    data = num;
 }
 
 Node::~Node()
