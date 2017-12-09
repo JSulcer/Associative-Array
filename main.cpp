8ee794fe17bb5d9f74ffa2298c6f00405484d993
JSulcer <34108753+JSulcer@users.noreply.github.com>
2017-12-08 21:06:53 -0600

    Updating main to test functionality

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-// Author: Jon Sulcer
+// Authors: Jon Sulcer and Adam Chisolm
 // Date: 12/2/17
 // File: main.cpp
-// Description:
+// Description: main driver for the associative array program
 
 #include "associative.h"
 using namespace std;
@@ -10,16 +10,32 @@ int main(){
     cout << "Hello World" << endl;
 
     associative Test;
+    string fileName;
 
     Test.insert("Bob", 5);
     Test.insert("Ted", 2);
     Test.insert("Nik", 7);
+    Test.insert("Bill", 15);
+    Test.insert("Zack", 12);
+    Test.insert("Hal", 17);
+    Test.insert("Adam", 11);
+    Test.insert("Jon", 55);
 
     Test.print();
 
     Test.delete_index("Ted");
 
     Test.print();
+    
+    Test.min();
+    Test.max();
+    
+    Test.find("Hal");
+    
+    cout << "Enter a file name: ";
+    cin >> fileName;
+    
+    Test.save_file(fileName);
     
     cout << "Done" << endl;
 
