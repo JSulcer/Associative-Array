f74f47bd481fc9ec7724390c00128535613f3fc0
JSulcer <34108753+JSulcer@users.noreply.github.com>
2017-12-08 23:14:23 -0600

    testing import

--- associative.cpp
+++ associative.cpp
@@ -225,13 +225,13 @@ void associative::importInsertHelper(Node *current, string value)
     for(unsigned int i = 0; i < value.length(); i++)
     {
       tolower(testValue[i]);
-      if(testValue < 'a' || testValue > 'z')
+      if(testValue[i] < 'a' || testValue[i] > 'z')
         testValue.erase(i);
     }
     for(unsigned int i = 0; i < testKey.length(); i++)
     {
       tolower(testKey[i]);
-      if(testKey < 'a' || testKey > 'z')
+      if(testKey[i] < 'a' || testKey[i] > 'z')
       	testKey.erase(i);
     }
     
